57ffdcc4f0e1d197ab9a1182c8ecafe006f8d417
Bustos Maximiliano <bustoss.maxi@gmail.com>
2025-08-04 22:14:32 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dengue_y_zika',
+    packages=find_packages(),
+    description='Explorar los factores que conllevan a la expansión del virus, principalmente relacionados con el mosquito Aedes aegypti (el vector), las condiciones climáticas y ambientales, y los patrones de movimiento humano.',
+    author='Bustos Maximiliano',
+    version='0.0.1'
+)
